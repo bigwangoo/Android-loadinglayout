462bf6e4c226b99b7369fc2b5a4dd8e15e153789
bigwangoo <467863144@qq.com>
2018-12-14 00:47:37 +0800

    loadinglayout

--- app/src/main/java/com/tianxiabuyi/ui/sample/MainActivity.java
+++ app/src/main/java/com/tianxiabuyi/ui/sample/MainActivity.java
@@ -2,17 +2,45 @@ package com.tianxiabuyi.ui.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-import ui.tianxiabuyi.com.loadinglayout.LoadingLayout;
+import com.tianxiabuyi.ui.loadinglayout.LoadingLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private LoadingLayout loadingLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LoadingLayout loadingLayout = LoadingLayout.wrap(this);
-        loadingLayout.showContent();
+
+        loadingLayout = findViewById(R.id.loading_layout);
+        TextView tv = findViewById(R.id.tv_success);
+
+        loadingLayout.setBindView(tv);
         loadingLayout.showLoading();
+    }
+
+    public void loading(View view) {
+        loadingLayout.showLoading();
+    }
+
+    public void empty(View view) {
+        loadingLayout.showEmpty();
+    }
+
+    public void success(View view) {
+        loadingLayout.showSuccess();
+    }
+
+    public void error(View view) {
+        loadingLayout.showError();
+    }
+
+    public void netError(View view) {
+        loadingLayout.showNetwork();
     }
 }
